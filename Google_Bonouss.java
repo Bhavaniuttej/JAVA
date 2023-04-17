cdb92e976553605bfad1768d21c0ec4c992a61e4
Bhavaniuttej <130923202+Bhavaniuttej@users.noreply.github.com>
2023-04-17 12:15:15 +0530

    Add files via upload

---
+++ Google_Bonouss.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Google_Bonouss
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Workingexperience in Google:");
+		int Workingexperience = scan.nextInt();
+		if(Workingexperience >=5)
+		{
+            System.out.println("Enter your Salary in Google:");
+			int Salary = scan.nextInt();
+			if (Salary<=70000)
+			{
+				System.out.println("Your bonous is :" +0.2*Salary);
+			}
+			else
+			{
+				System.out.println("your not eligible for bonous");
+			}
+		}
+		else
+		{
+			System.out.println(" your not eligible for bonous:");
+		}
+	}
+}
+
