3becd71d4a316d0e8f324f0b86b2d78e5dead78a
Bhavaniuttej <130923202+Bhavaniuttej@users.noreply.github.com>
2023-04-20 10:41:25 +0530

    Add files via upload

---
+++ VotingEligibility.java
@@ -0,0 +1,9 @@
+class VotingEligibility 
+{
+	public static void main(String[] args) 
+	{
+		int age = 40;
+		String result = (age>=18)?"Eligible for voting":"Not Eligible for voting";
+		System.out.println(result);
+	}
+}
